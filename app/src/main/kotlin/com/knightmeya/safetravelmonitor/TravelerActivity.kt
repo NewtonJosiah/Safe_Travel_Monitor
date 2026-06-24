931b9431654af08a310614cb9c62d6d432605d38
Newton Josiah <newtonjosiah1@gmail.com>
2026-06-24 08:59:50 +0300

    Migrate to built-in Kotlin support for AGP 9.0+ and fix missing Intent import

--- app/src/main/kotlin/com/knightmeya/safetravelmonitor/TravelerActivity.kt
+++ app/src/main/kotlin/com/knightmeya/safetravelmonitor/TravelerActivity.kt
@@ -1,6 +1,7 @@
 package com.knightmeya.safetravelmonitor
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.PointF
 import android.os.Bundle
